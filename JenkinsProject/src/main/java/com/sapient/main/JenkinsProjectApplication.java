b45c1c6f3412a3c23b385c1913e9d52bf94d8231
Manjeet Singh <manjeet.singh1@publicissapient.com>
2019-08-19 16:34:13 +0530

    Added test

--- JenkinsProject/src/main/java/com/sapient/main/JenkinsProjectApplication.java
+++ JenkinsProject/src/main/java/com/sapient/main/JenkinsProjectApplication.java
@@ -3,11 +3,16 @@ package com.sapient.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class JenkinsProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsProjectApplication.class, args);
+		Sum s=null;
+		System.out.println(s.add(5,9));
+		System.out.println(s.add(6,9));
 	}
 
 }
--- JenkinsProject/src/test/java/com/sapient/main/JenkinsProjectApplicationTests.java
+++ JenkinsProject/src/test/java/com/sapient/main/JenkinsProjectApplicationTests.java
@@ -1,5 +1,9 @@
+
 package com.sapient.main;
 
+import static org.junit.Assert.assertEquals;
+
+import org.apache.catalina.filters.AddDefaultCharsetFilter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,9 +12,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JenkinsProjectApplicationTests {
-
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+	
 	@Test
-	public void contextLoads() {
+	public void testAdd() {
+		Sum s=null;
+		assertEquals(15, s.add(10, 5));
+		
 	}
 
 }
